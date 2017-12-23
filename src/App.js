85bee87159d1e851912e09c014da01b3d489da58
Suvam Pramanik <pramaniksuvam@yahoo.com>
2017-12-24 01:55:36 +0530

    Show up temperature graph based on location

--- src/App.js
+++ src/App.js
@@ -1,21 +1,128 @@
-import logo from './logo.svg';
 import React, {Component} from 'react';
-import "./App.css';
+import xhr from 'xhr';
+import './App.css';
+import ErrorPage from './ErrorPage.js';
+import Plot from './Plot.js';
+
+const API_KEY = '50ebb139a30adf57eb66817bf692b98e';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      fetchError: false,
+      location: '',
+      data: {},
+      dataes: [],
+      temps: [],
+      dataSelected: {
+        date: '',
+        temp: null
+      }
+    };
+  }
+
+  fetchWeatherData = (evt) => {
+    evt.preventDefault();
+    const location = encodeURIComponent(this.state.location);
+    const apiPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+    const apiSuffix = '&APPID=' + API_KEY + '&units=metric';
+    const api = apiPrefix + location + apiSuffix;
+
+    // Used for getting the reference inside other components
+    const self = this;
+
+    xhr({
+      url: api
+    }, function (err, data) {
+      if (data.statusCode === 404) {
+        console.log(data);
+        self.setState({
+          fetchError: true
+        });
+      } else {
+        var body = JSON.parse(data.body);
+        var dates = [];
+        var temps = [];
+        for (var item of body.list) {
+          dates.push(item.dt_txt);
+          temps.push(item.main.temp);
+        }
+        self.setState({
+          fetchError: false,
+          data: body,
+          dates: dates,
+          temps: temps,
+          dataSelected: {
+            data: '',
+            temp: null
+          }
+        });
+      }
+    });
+  }
+
+  updateLocation = (evt) => {
+    this.setState({
+      location: evt.target.value
+    });
+  }
+
+  onPlotClick = (data) => {
+    if (data.points) {
+      this.setState({
+        dataSelected: {
+          date: data.points[0].x,
+          temp: data.points[0].y
+        }
+      });
+    }
+  }
+
   render () {
+    let currTemp = 'Loading weather data....';
+    if (this.state.data.list) {
+      currTemp = this.state.data.list[0].main.temp;
+    }
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={'weather-div'}>
+        <h1>Weather Page</h1>
+        <form onSubmit={this.fetchWeatherData}>
+          <label>I want to know the weather for
+            <input
+              id={'place-input'}
+              placeholder={'City, Country'}
+              type='text'
+              value={this.state.location}
+              onChange={this.updateLocation} />
+          </label>
+        </form>
+        {/*
+          Render the current temperature and the forecast if we have data
+          otherwise return null
+        */}
+        { (this.state.fetchError) ? (
+          <ErrorPage location={this.state.location} />
+        ) : (this.state.data.list) ? (
+          <div className='wrapper'>
+            <p className='temp-wrapper'>
+              <span className='temp'>
+                {this.state.dataSelected.temp ? this.state.dataSelected.temp : currTemp}
+              </span>
+              <span className='temp-symbol'>°C</span>
+              <span className='temp-date'>
+                { this.state.dataSelected.temp ? this.state.dataSelected.date : ''}
+              </span>
+            </p>
+            <h2>Forecast</h2>
+            <Plot
+              xData={this.state.dates}
+              yData={this.state.temps}
+              type='scatter'
+              onPlotClick={this.onPlotClick} />
+          </div>) : null }
       </div>
     );
   }
 }
-
 export default App;
