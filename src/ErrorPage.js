e87c2586236fc0f5cd2a0303644330d0ba242901
Suvam Pramanik <pramaniksuvam@yahoo.com>
2017-12-24 02:21:41 +0530

    Adding alt text to the error image

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -7,7 +7,7 @@ class ErrorPage extends Component {
       <div>
         <span className='error-text'>No Weather data found for location <b> {this.props.location} </b> </span>
         <span className='error-image' >
-          <img id='sad-img' src={'./assets/sad_emoji.png'} />
+          <img id='sad-img' src={'./assets/sad_emoji.png'} alt='' />
         </span>
         <p> Please try again! </p>
       </div>, document.getElementById('error-wrapper')
